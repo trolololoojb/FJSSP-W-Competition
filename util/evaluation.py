f899d1b2943a1644d65f80038594fa664f52b17b
dhutter-fhv <david.hutter@fhv.at>
2026-04-20 09:37:09 +0200

    update evaluation

--- util/evaluation.py
+++ util/evaluation.py
@@ -6,9 +6,9 @@ def makespan(start_times : list[int], machine_assignments : list[int], worker_as
 
 def workload_balance(machine_assignments : list[int], worker_assignments : list[int], durations : list[list[list[int]]]) -> float:
     n_workers = max(worker_assignments)
-    working_time = [0] * n_workers
+    working_time = [0] * (n_workers+1)
     for i in range(len(worker_assignments)):
-        working_time[worker_assignments[i]-1] += durations[i][machine_assignments[i]][worker_assignments[i]]
+        working_time[worker_assignments[i]] += durations[i][machine_assignments[i]][worker_assignments[i]]
     mean_working_time = np.mean(working_time)
     result = 0.0
     for i in range(len(working_time)):
